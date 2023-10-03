ee90598567d8e5bc8c60ff0a2b0ab793ef8a400e
anbarasan <mechanbu42@gmail.com>
2023-10-04 00:08:21 +0530

    anb

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const  App =  ()=>{
         {/* <Router>
             <Routes>
                 <Route path="/" element={<Auth/>} />
-                <Route path="/home" element={<Home/>} />  
+                <Route path="/home" element={<Home/>} />    
             </Routes>
         </Router> */}
         {
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: "AIzaSyCECmhyjaNWRYa8Cnyu4VUCt2GBYz0UCeg",
     authDomain: "fir-project-122af.firebaseapp.com",
-    projectId: "fir-project-122af",
+  projectId: "fir-project-122af",  
     storageBucket: "fir-project-122af.appspot.com",
     messagingSenderId: "852818610234",
     appId: "1:852818610234:android:17093650188c58c8c1cd03"
